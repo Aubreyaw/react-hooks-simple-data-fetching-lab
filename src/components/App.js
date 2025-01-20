e6a1c6f616e7e0005b483723f085ed056f8ee34d
aubrey whitford <aubreya.whitford@gmail.com>
2025-01-20 17:11:33 -0500

    Completes lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,34 @@
-// create your App component here
+import React, { useState, useEffect} from "react";
+
+const dogApi = "https://dog.ceo/api/breeds/image/random";
+
+function App() {
+    const [image, setImage] = useState(null);
+
+    useEffect(() => {
+        fetch(dogApi)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Fetched image URL:', data.message); 
+                setImage(data.message);
+            })
+            .catch(error => console.error('Error fetching the image:', error));
+    }, []);
+
+    if (!image) {
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div>
+            <img src={image} alt="A Random Dog"></img>
+        </div>
+    );
+}
+export default App
+
+// useEffect(() => {
+    //     fetch(dogApi)
+    //     .then(r => r.json())
+    //     .then(data => setImage(data.image))
+    // }, [])
